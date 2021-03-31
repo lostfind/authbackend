e44a6c7c43e6e0c426481c528a17f754ffccbe40
lostfind <kpdzzang@gmail.com>
2021-03-31 23:07:57 +0900

    login customToken

--- go.mod
+++ go.mod
@@ -6,6 +6,12 @@ require (
 	cloud.google.com/go/firestore v1.5.0 // indirect
 	cloud.google.com/go/storage v1.14.0 // indirect
 	firebase.google.com/go v3.13.0+incompatible
+	github.com/gin-contrib/cors v1.3.1 // indirect
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	golang.org/x/oauth2 v0.0.0-20210323180902-22b0adad7558 // indirect
 	google.golang.org/api v0.43.0
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
 )
--- main.go
+++ main.go
@@ -4,58 +4,115 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/http"
 
 	firebase "firebase.google.com/go"
 	"firebase.google.com/go/auth"
-
-	"google.golang.org/api/option"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-
-	ctx := context.Background()
-
-	app, err := initFirebase()
+	firebaseApp, err := initFirebase()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	client, err := app.Auth(context.Background())
+	firebaseClient, err := firebaseApp.Auth(context.Background())
 	if err != nil {
 		log.Fatalf("error getting Auth client: %v\n", err)
 	}
 
-	user := createUser(ctx, client)
+	r := gin.Default()
 
-	log.Print(user)
+	r.Use(cors.New(cors.Config{
+		// 許可したいHTTPメソッドの一覧
+		AllowMethods: []string{
+			"POST",
+			"GET",
+			"OPTIONS",
+			"PUT",
+			"DELETE",
+		},
+		// 許可したいHTTPリクエストヘッダの一覧
+		AllowHeaders: []string{
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"X-CSRF-Token",
+			"Authorization",
+		},
+		AllowOrigins: []string{
+			"http://localhost:3000",
+		},
+	}))
 
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.POST("/createUser", func(c *gin.Context) {
+		params := (&auth.UserToCreate{}).
+			Email(c.PostForm("email")).
+			EmailVerified(false).
+			Password(c.PostForm("password")).
+			Disabled(false)
+
+		u, err := firebaseClient.CreateUser(c.Request.Context(), params)
+		if err != nil {
+			log.Fatalf("error creating user: %v\n", err)
+		}
+		log.Printf("Successfully created user: %v\n", u)
+
+		c.JSON(200, gin.H{
+			"user": u.UID,
+		})
+	})
+
+	type JsonRequest struct {
+		Email string `json:"email"`
+	}
+
+	r.POST("/login", func(c *gin.Context) {
+		ctx := c.Request.Context()
+
+		var json JsonRequest
+		if err := c.ShouldBindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		email := json.Email
+
+		u, err := firebaseClient.GetUserByEmail(ctx, email)
+		if err != nil {
+			log.Printf("error getting user by email %s: %v\n", email, err)
+		}
+
+		log.Printf("Successfully fetched user data: %v\n", u)
+
+		token, err := firebaseClient.CustomToken(ctx, u.UID)
+
+		if err != nil {
+			log.Printf("error minting custom token: %v\n", err)
+		}
+
+		c.JSON(200, gin.H{
+			"token": token,
+		})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
 func initFirebase() (*firebase.App, error) {
-	opt := option.WithCredentialsFile("key/firebase-adminsdk.json")
-	app, err := firebase.NewApp(context.Background(), nil, opt)
+	app, err := firebase.NewApp(context.Background(), nil)
 	if err != nil {
 		return nil, fmt.Errorf("error initializing app: %v", err)
 	}
 
 	return app, err
-}
-
-func createUser(ctx context.Context, client *auth.Client) *auth.UserRecord {
-	// [START create_user_golang]
-	params := (&auth.UserToCreate{}).
-		Email("user@example.com").
-		EmailVerified(false).
-		PhoneNumber("+15555550100").
-		Password("secretPassword").
-		DisplayName("John Doe").
-		PhotoURL("http://www.example.com/12345678/photo.png").
-		Disabled(false)
-	u, err := client.CreateUser(ctx, params)
-	if err != nil {
-		log.Fatalf("error creating user: %v\n", err)
-	}
-	log.Printf("Successfully created user: %v\n", u)
-	// [END create_user_golang]
-	return u
 }
